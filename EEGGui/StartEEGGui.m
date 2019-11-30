2a69ee0e96c706c84a50fb911926e186c11cee36
FabianNowak <58370948+FabianNowak@users.noreply.github.com>
2019-11-30 20:33:41 +0100

    rename EEGGui/ to src/

--- EEGGui/StartEEGGui.m
+++
@@ -1,6 +0,0 @@
-%MATLAB Apps require the main function to return the figure handle
-%StartEEGGui() opens the App and extracts the underlying figure
-function fig = StartEEGGui()
-    gui = EEGGui;
-    fig = gui.fig;
-end
